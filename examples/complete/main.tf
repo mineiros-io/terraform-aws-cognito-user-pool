1f7943bd822e85feb0f4df4c801b7d680d640350
Marius Tolzmann <m@rius.berlin>
2020-09-16 19:02:28 +0200

    Prepare 0.2.0 release

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 
 module "cognito_user_pool" {
   source  = "mineiros-io/cognito-user-pool/aws"
-  version = "~> 0.1.4"
+  version = "~> 0.2.0"
 
   name = "complete-example-userpool"
 
