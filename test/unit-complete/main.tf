985e154d340c4b1c683f1b4c02f3a8f3fab1263c
Soren Martius <soeren.martius@gmail.com>
2021-10-28 11:42:53 +0200

    test: implement new test structure for disabled, minimum and complete tests

---
+++ test/unit-complete/main.tf
@@ -0,0 +1,161 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# COMPLETE FEATURES UNIT TEST
+# This module tests a complete set of most/all non-exclusive features
+# The purpose is to activate everything the module offers, but trying to keep execution time and costs minimal.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+variable "aws_region" {
+  description = "(Optional) The AWS region in which all resources will be created."
+  type        = string
+  default     = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.50"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# DO NOT RENAME MODULE NAME
+module "test" {
+  source = "../.."
+
+  module_enabled = true
+
+  # add all required arguments
+  name = "example-cognito-user-pool"
+
+  # add all optional arguments that create additional resources
+  clients = [
+    {
+      name                 = "android-mobile-client"
+      read_attributes      = ["email", "email_verified", "preferred_username"]
+      allowed_oauth_scopes = ["email", "openid"]
+      allowed_oauth_flows  = ["implicit"]
+      callback_urls        = ["https://mineiros.io/callback", "https://mineiros.io/anothercallback"]
+      default_redirect_uri = "https://mineiros.io/callback"
+      generate_secret      = true
+    }
+  ]
+
+  # add most/all other optional arguments
+
+  enable_username_case_sensitivity = false
+  advanced_security_mode           = "ENFORCED"
+
+  alias_attributes = [
+    "email",
+    "phone_number",
+    "preferred_username",
+  ]
+
+  auto_verified_attributes = [
+    "email"
+  ]
+
+  account_recovery_mechanisms = [
+    {
+      name     = "verified_email"
+      priority = 1
+    },
+    {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+  ]
+
+  # If invited by an admin
+  invite_email_subject = "You've been invited to Mineiros.io"
+  invite_email_message = "Hi {username}, your temporary password is '{####}'."
+  invite_sms_message   = "Hi {username}, your temporary password is '{####}'."
+
+  # domain                = "mineiros-dev"
+  default_email_option  = "CONFIRM_WITH_LINK"
+  email_subject_by_link = "Your Verification Link"
+  email_message_by_link = "Please click the link below to verify your email address. {##Verify Email##}."
+  sms_message           = "Your verification code is {####}."
+
+  challenge_required_on_new_device = true
+  user_device_tracking             = "USER_OPT_IN"
+
+  # These paramters can be used to configure SES for emails
+  # email_sending_account  = "DEVELOPER"
+  # email_reply_to_address = "support@mineiros.io"
+  # email_from_address     = "noreply@mineiros.io"
+  # email_source_arn       = "arn:aws:ses:us-east-1:999999999999:identity"
+
+  # Require MFA
+  mfa_configuration        = "ON"
+  allow_software_mfa_token = true
+
+  password_minimum_length    = 40
+  password_require_lowercase = true
+  password_require_numbers   = true
+  password_require_uppercase = true
+  password_require_symbols   = true
+
+  temporary_password_validity_days = 3
+
+  schema_attributes = [
+    {
+      name       = "gender", # overwrites the default attribute 'gender'
+      type       = "String"
+      required   = true
+      min_length = 1
+      max_length = 2048
+    },
+    {
+      name                     = "alternative_name"
+      type                     = "String"
+      developer_only_attribute = false,
+      mutable                  = true,
+      required                 = false,
+      min_length               = 0,
+      max_length               = 2048
+    },
+    {
+      name      = "friends_count"
+      type      = "Number"
+      min_value = 0,
+      max_value = 100
+    },
+    {
+      name = "is_active"
+      type = "Boolean"
+
+    },
+    {
+      name = "last_seen"
+      type = "DateTime"
+    }
+  ]
+
+  default_client_token_validity_units = {
+    refresh_token = "hours"
+    access_token  = "hours"
+    id_token      = "hours"
+  }
+
+  tags = {
+    Team = "Unknown"
+  }
+
+  module_tags = {
+    Environment = "unknown"
+  }
+
+  module_depends_on = ["nothing"]
+}
+
+# outputs generate non-idempotent terraform plans so we disable them for now unless we need them.
+# output "all" {
+#   description = "All outputs of the module."
+#   value       = module.test
+# }
---
+++ test/unit-disabled/main.tf
@@ -0,0 +1,43 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# EMPTY FEATURES (DISABLED) UNIT TEST
+# This module tests an empty set of features.
+# The purpose is to verify no resources are created when the module is disabled.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+variable "aws_region" {
+  description = "(Optional) The AWS region in which all resources will be created."
+  type        = string
+  default     = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.50"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# DO NOT RENAME MODULE NAME
+module "test" {
+  source = "../.."
+
+  name = "example-cognito-user-pool"
+
+  module_enabled = false
+
+  # add all required arguments
+
+  # add all optional arguments that create additional resources
+}
+
+# outputs generate non-idempotent terraform plans so we disable them for now unless we need them.
+# output "all" {
+#   description = "All outputs of the module."
+#   value       = module.test
+# }
